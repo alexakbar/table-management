3c29aa76437d7d2e911d41d13542fdd22c1d14c9
alexakbar <alexampp11@gmail.com>
2024-03-17 02:14:02 +0700

    (fix) delete helper not usage

--- src/main/java/id/alex/helpers/UtilsHelper.java
+++ src/main/java/id/alex/helpers/UtilsHelper.java
@@ -1,11 +1,6 @@
 package id.alex.helpers;
 
 public class UtilsHelper {
-
-    public static boolean isEmpty(String text){
-        return text == null || text.isBlank();
-    }
-
     public static boolean isNullOrEmpty(String str) {
         return str == null || str.equalsIgnoreCase("");
     }
